05b931644b0bc1d5abdbb4c565ab807daaa99f3b
emircan özer <m.emircanozer@outlook.com>
2026-02-23 16:45:28 +0300

    add login screen

---
+++ Bankey/Login/LoginView.swift
@@ -0,0 +1,100 @@
+//
+//  LoginView.swift
+//  Bankey
+//
+//  Created by Emircan Özer on 23.02.2026.
+// daha sonra değişecebilecek komponentleri burada tanımladık Başka bir sayfada da kullanabilirsin.
+
+import Foundation
+import UIKit
+
+class LoginView: UIView {
+    
+    //clousresuz tanımladık farketmiyor stili aşağıda tanımladık 
+    let stackView = UIStackView()
+    let usernameTextField = UITextField()
+    let passwordTextField = UITextField()
+    let dividerView = UIView()
+    
+    // initler zorunlu no storyboard için
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        style()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //"Benim doğal bir boyutum yok, boyutumu constraint'lerden hesapla."noIntrinsicMetric = "bu boyut için bir değerim yok" demek.
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: UIView.noIntrinsicMetric, height: UIView.noIntrinsicMetric)
+    }
+}
+
+
+// stil ve layout işlemlerini extension ile tanımlıyoruz
+extension LoginView {
+    
+    private func style() {
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .secondarySystemBackground
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        
+        usernameTextField.translatesAutoresizingMaskIntoConstraints = false
+        usernameTextField.placeholder = "Username"
+        usernameTextField.delegate = self
+
+        dividerView.translatesAutoresizingMaskIntoConstraints = false
+        dividerView.backgroundColor = .secondarySystemFill
+
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        passwordTextField.placeholder = "Password"
+        passwordTextField.isSecureTextEntry = true
+        passwordTextField.delegate = self
+                
+        layer.cornerRadius = 5
+        clipsToBounds = true
+    }
+    
+    private func layout() {
+        stackView.addArrangedSubview(usernameTextField)
+        stackView.addArrangedSubview(dividerView)
+        stackView.addArrangedSubview(passwordTextField)
+
+        addSubview(stackView)
+        
+        // StackView
+        NSLayoutConstraint.activate([  //systemspacingbelow apple in izin verdiği boşluk aralığı multiper de 8 x olarak boşluk veriyor
+            stackView.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 1),
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 1),
+            trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 1),
+            bottomAnchor.constraint(equalToSystemSpacingBelow: stackView.bottomAnchor, multiplier: 1)
+        ])
+        
+        dividerView.heightAnchor.constraint(equalToConstant: 1).isActive = true
+    }
+}
+
+// textfield için delegete zorunlu değil ama klavye kapanması için kullandık
+extension LoginView: UITextFieldDelegate {
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        usernameTextField.endEditing(true)
+        passwordTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+    }
+}
+    
