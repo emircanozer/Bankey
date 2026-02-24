d159c692466ee0c3e2c7f8ae33e17770ec6f5d03
emircan özer <m.emircanozer@outlook.com>
2026-02-24 10:15:40 +0300

    add Onboarding screen

---
+++ Bankey/Onboarding/OnboardingContainerViewController.swift
@@ -0,0 +1,123 @@
+//
+//  OnboardingContainerViewController.swift
+//  Bankey
+//
+//  Created by Emircan Özer on 24.02.2026.
+//
+
+import UIKit
+
+//main container tüm sistemi yöneten class
+class OnboardingContainerViewController: UIViewController {
+
+    let pageViewController: UIPageViewController //scrollable,kalbi
+    var pages = [UIViewController]()
+    var currentVC: UIViewController {
+        //bir property'nin değeri her değiştiğinde otomatik çalışan blok.
+        didSet {
+        }
+    }
+    
+    //nib bundle gibi parametreler zorunlu olduğu için var
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        self.pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+        
+        let page1 = ViewController1()
+        let page2 = ViewController2()
+        let page3 = ViewController3()
+        
+        pages.append(page1)
+        pages.append(page2)
+        pages.append(page3)
+        
+        currentVC = pages.first!
+        
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPurple
+        
+        /*sen ekrana geldiğinde child da gelsin, sen kapandığında child da kapansın, sen döndüğünde child da dönsün. iOS bunu otomatik yönetsin.
+        Bunu yapmazsan child kör ve sağır olur. Ekranda görünür ama sistemden kopuktur. */
+        addChild(pageViewController)
+        //Çocuğun view'ını ekrana ekliyorsun. Yani görsel olarak ekranda beliriyor
+        view.addSubview(pageViewController.view)
+        //lifecycle eventlerini düzgün almaya başlıyor. bu 3ü beraber kullanılır
+
+        pageViewController.didMove(toParent: self)
+        
+        pageViewController.dataSource = self
+        pageViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            view.topAnchor.constraint(equalTo: pageViewController.view.topAnchor),
+            view.leadingAnchor.constraint(equalTo: pageViewController.view.leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: pageViewController.view.trailingAnchor),
+            view.bottomAnchor.constraint(equalTo: pageViewController.view.bottomAnchor),
+        ])
+        
+        pageViewController.setViewControllers([pages.first!], direction: .forward, animated: false, completion: nil)
+        currentVC = pages.first!
+    }
+}
+
+// MARK: - UIPageViewControllerDataSource protokolü, kullanıcı sağa veya sola kaydırdığında "Sırada ne var?" sorusuna yanıt verir.
+extension OnboardingContainerViewController: UIPageViewControllerDataSource {
+
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        return getPreviousViewController(from: viewController)
+    }
+
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        return getNextViewController(from: viewController)
+    }
+
+    private func getPreviousViewController(from viewController: UIViewController) -> UIViewController? {
+        guard let index = pages.firstIndex(of: viewController), index - 1 >= 0 else { return nil }
+        currentVC = pages[index - 1]
+        return pages[index - 1]
+    }
+
+    private func getNextViewController(from viewController: UIViewController) -> UIViewController? {
+        guard let index = pages.firstIndex(of: viewController), index + 1 < pages.count else { return nil }
+        currentVC = pages[index + 1]
+        return pages[index + 1]
+    }
+
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return pages.count
+    }
+
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        return pages.firstIndex(of: self.currentVC) ?? 0
+    }
+}
+
+// MARK: - ViewControllers
+class ViewController1: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+}
+
+class ViewController2: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+    }
+}
+
+class ViewController3: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
